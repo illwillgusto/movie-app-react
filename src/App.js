c1c7320886a21613bc4300064486e0ddff96d180
Will Walton <will@endless3dge.com>
2024-03-29 13:37:03 -0500

    Update App.js
    import path for the following links
    link to redux
    link to users authentication
    link to main scss file in the scss folder
    
    AppRouter link import path as well

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useEffect } from "react";
 
-import { connect } from "react-redux";
+import { connect } from "react-redux"; 
+
+import { checkUserAuthentication } from "./store/users/actions";
+import "./scss/main.scss"
+import AppRouter from ",/AppRouter";
 
 
