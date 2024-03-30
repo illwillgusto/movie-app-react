3549361fb84c5e2f9c850e551e61819bf4e2786a
Will Walton <will@endless3dge.com>
2024-03-30 02:04:03 -0500

    Update App.js
    code for app.js added
    hook for checking the user's authentication added
    
    return statement for the props added
    
    dispatch added as well

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import { checkUserAuthentication } from "./store/users/actions";
 import "./scss/main.scss"
 import AppRouter from ",/AppRouter";
 
+const App = (props) => {
+    useEffect(() => {
+        props.checkUserAuthentication();
 
+    }, []);
+    return <AppRouter {...props} />;
+};
 
+const mapDispatchToProps = (dispatch) => ({
+    checkUserAuthentication: () => dispatch(checkUserAuthentication()),
+});
 
+export default connect(null, mapDispatchToProps)(App);
